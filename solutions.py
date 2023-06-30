846eddcdbdbacc406e0b775698349caf7ce05770
Karan Wadhwani <kwadhwani@xpanxion.com>
2023-06-30 14:21:26 -0400

    starting ex 8

--- solutions.py
+++ solutions.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 from src.WordCounter import WordCounter
 from src.TaxMan import TaxMan
+from src.Calculator import Calculator 
 
 # def ex1():
 #     people_list = [
@@ -36,3 +37,23 @@ def ex6():
 
 if __name__ == '__main__':
     ex6()
+
+def ex7():
+    calculator1 = Calculator(4, 3)
+    calculator1.add()
+    print(calculator1.get_result())
+
+    calculator2 = Calculator(4, 3)
+    calculator2.sub()
+    print(calculator2.get_result())
+
+    calculator3 = Calculator(2, 3)
+    calculator3.mul()
+    print(calculator3.get_result())
+
+    calculator4 = Calculator(8, 2)
+    calculator4.div()
+    print(calculator4.get_result())
+
+if __name__ == '__main__':
+    ex7()
